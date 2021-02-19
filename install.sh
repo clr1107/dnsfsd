71a973e52c5fafa4dced53d837d690237b5549bc
clr1107 <chris@clr.is>
2021-02-19 18:43:54 +0000

    First version of the install script, definitely will not stay like this for long.

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+    echo "Run this script as root: sudo bash install.sh"
+    exit 1
+fi
+
+echo "Creating necessary directories"
+mkdir -p /etc/dnsfd/patterns
+
+if [ $? -ne 0 ]; then
+    echo "Could not create directories /etc/dnsfsd/..."
+    exit 1
+fi
+
+echo "Moving binary & updating permissions"
+mv dnsfsd /bin/dnsfsd
+chmod +x /bin/dnsfsd
+
+if [ $? -ne 0 ]; then
+    echo "Could not move `dnsfsd` to `/bin/dnsfsd`"
+    exit 1
+fi
