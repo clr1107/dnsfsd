418961266b956919942ebb949098598832067c1c
clr1107 <chris@clr.is>
2021-02-20 16:52:05 +0000

    Implemented the server; all that's left is the forwarding. At the moment it goes to forward and just sinks all.

--- daemon/main.go
+++ daemon/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/clr1107/dnsfsd/daemon/server"
+)
+
+func main() {
+	srv := &server.DNSFSServer{Port: 53}
+
+	if err := srv.Start(); err != nil {
+		fmt.Printf("starting error: %v\n", err)
+		return
+	}
+
+	println("serving...")
 }
--- daemon/server/server.go
+++ daemon/server/server.go
@@ -35,9 +35,61 @@ type handler struct {
 }
 
 func newHandler(patterns []*regexp.Regexp) *handler {
-	return &handler{patterns, persistence.NewSimpleCache(120000)}
+	return &handler{patterns, persistence.NewSimpleCache(-1)}
+}
+
+// true => sink; false => nothing found
+func (h *handler) checkPatterns(domain string) bool {
+	for _, pattern := range h.patterns {
+		if pattern.MatchString(domain) {
+			return true
+		}
+	}
+
+	return false
+}
+
+// returns whether to sink or not based on cache and pattern matching
+func (h *handler) check(domain string) bool {
+	if h.cache.Contains(domain) {
+		if val, ok := h.cache.Get(domain).(bool); ok {
+			return val
+		}
+
+		h.cache.Remove(domain) // for some reason not a bool?
+	}
+
+	if h.checkPatterns(domain) {
+		h.cache.PutDefault(domain, true)
+		return true
+	}
+
+	return false
+}
+
+func (h *handler) forward(w *dns.ResponseWriter, r *dns.Msg, msg *dns.Msg, dnsIP string) {
+	// msg.Answer = append(msg.Answer, &dns.A{
+	// 	Hdr: dns.RR_Header{Name: domain, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 60},
+	// 	A:   net.ParseIP(result),
+	// })
+	(*w).WriteMsg(msg) // todo implement forwarding...
 }
 
 func (h *handler) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
-	// todo
+	aType := r.Question[0].Qtype == dns.TypeA
+
+	msg := dns.Msg{}
+	msg.SetReply(r)
+	msg.Authoritative = true
+
+	if aType {
+		domain := msg.Question[0].Name
+
+		if h.check(domain) {
+			w.WriteMsg(&msg) // just sink right now
+			return
+		}
+	}
+
+	go h.forward(&w, r, &msg, "1.1.1.1")
 }
