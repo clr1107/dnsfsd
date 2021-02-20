5b4ce62f2da88b779dcf295a80df5d059fd5cae5
clr1107 <chris@clr.is>
2021-02-20 17:22:50 +0000

    Implemented forwarding to 1.1.1.1 This value will be read fro, the cnofiguration in the future.

--- daemon/main.go
+++ daemon/main.go
@@ -2,17 +2,44 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/clr1107/dnsfsd/daemon/server"
 )
 
 func main() {
+	// this will obv be touched up a little.
 	srv := &server.DNSFSServer{Port: 53}
 
-	if err := srv.Start(); err != nil {
-		fmt.Printf("starting error: %v\n", err)
+	if err := srv.Init(); err != nil {
+		fmt.Printf("init error: %v\n", err)
 		return
 	}
 
-	println("serving...")
+	signalChannel := make(chan os.Signal, 1)
+	signal.Notify(signalChannel, os.Interrupt, syscall.SIGTERM, os.Kill)
+
+	go func() {
+		<-signalChannel
+		fmt.Printf("\nShutting down...\n")
+
+		if err := srv.Shutdown(); err != nil {
+			fmt.Printf("Error whilst shutting down: %v\n", err)
+		}
+
+		os.Exit(0)
+	}()
+
+	go func() {
+		for error := range srv.Handler.ErrorChannel {
+			fmt.Printf("Server Error: %v\n", error)
+		}
+	}()
+
+	if err := srv.Server.ListenAndServe(); err != nil {
+		fmt.Printf("server error whilst starting: %v\n", err)
+		return
+	}
 }
--- daemon/server/server.go
+++ daemon/server/server.go
@@ -1,8 +1,11 @@
 package server
 
 import (
+	"errors"
+	"fmt"
 	"regexp"
 	"strconv"
+	"strings"
 
 	"github.com/clr1107/dnsfsd/pkg/persistence"
 	"github.com/miekg/dns"
@@ -10,12 +13,12 @@ import (
 
 type DNSFSServer struct {
 	Port    int
-	server  *dns.Server
-	handler *handler
+	Server  *dns.Server
+	Handler *DNSFSHandler
 }
 
-func (s *DNSFSServer) Start() error {
-	s.server = &dns.Server{Addr: ":" + strconv.Itoa(s.Port), Net: "udp"}
+func (s *DNSFSServer) Init() error {
+	s.Server = &dns.Server{Addr: ":" + strconv.Itoa(s.Port), Net: "udp"}
 
 	files, err := persistence.LoadAllPatternFiles("/etc/dnsfsd/patterns")
 	if err != nil {
@@ -23,23 +26,31 @@ func (s *DNSFSServer) Start() error {
 	}
 
 	patterns := persistence.CollectAllPatterns(files)
-	s.handler = newHandler(patterns) // just copy it accross to our own struct.
+	s.Handler = newHandler(patterns) // just copy it accross to our own struct.
 
-	s.server.Handler = s.handler
-	return s.server.ListenAndServe()
+	s.Server.Handler = s.Handler
+	return nil
 }
 
-type handler struct {
-	patterns []*regexp.Regexp
-	cache    *persistence.SimpleCache
+func (s *DNSFSServer) Shutdown() error {
+	s.Handler.cache.Clear()
+	close(s.Handler.ErrorChannel)
+
+	return s.Server.Shutdown()
 }
 
-func newHandler(patterns []*regexp.Regexp) *handler {
-	return &handler{patterns, persistence.NewSimpleCache(-1)}
+type DNSFSHandler struct {
+	patterns     []*regexp.Regexp
+	cache        *persistence.SimpleCache
+	ErrorChannel chan error
+}
+
+func newHandler(patterns []*regexp.Regexp) *DNSFSHandler {
+	return &DNSFSHandler{patterns, persistence.NewSimpleCache(-1), make(chan error)}
 }
 
 // true => sink; false => nothing found
-func (h *handler) checkPatterns(domain string) bool {
+func (h *DNSFSHandler) checkPatterns(domain string) bool {
 	for _, pattern := range h.patterns {
 		if pattern.MatchString(domain) {
 			return true
@@ -50,7 +61,7 @@ func (h *handler) checkPatterns(domain string) bool {
 }
 
 // returns whether to sink or not based on cache and pattern matching
-func (h *handler) check(domain string) bool {
+func (h *DNSFSHandler) check(domain string) bool {
 	if h.cache.Contains(domain) {
 		if val, ok := h.cache.Get(domain).(bool); ok {
 			return val
@@ -67,29 +78,43 @@ func (h *handler) check(domain string) bool {
 	return false
 }
 
-func (h *handler) forward(w *dns.ResponseWriter, r *dns.Msg, msg *dns.Msg, dnsIP string) {
-	// msg.Answer = append(msg.Answer, &dns.A{
-	// 	Hdr: dns.RR_Header{Name: domain, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 60},
-	// 	A:   net.ParseIP(result),
-	// })
-	(*w).WriteMsg(msg) // todo implement forwarding...
+func (h *DNSFSHandler) forward(w *dns.ResponseWriter, r *dns.Msg, dnsAddress string) {
+	c := new(dns.Client)
+	x, _, err := c.Exchange(r, dnsAddress)
+
+	if strings.Contains(r.Question[0].Name, "errorpls") {
+		err = errors.New("random err")
+	}
+
+	if err != nil || x == nil {
+		if x == nil {
+			err = fmt.Errorf("after forwarding to '%v' the message response was nil", dnsAddress)
+		}
+
+		h.ErrorChannel <- err
+
+		x = &dns.Msg{}
+		x.SetReply(r)
+		x.Authoritative = r.Authoritative
+	}
+
+	(*w).WriteMsg(x)
 }
 
-func (h *handler) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
+func (h *DNSFSHandler) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
 	aType := r.Question[0].Qtype == dns.TypeA
 
-	msg := dns.Msg{}
-	msg.SetReply(r)
-	msg.Authoritative = true
-
 	if aType {
-		domain := msg.Question[0].Name
+		msg := dns.Msg{}
+		msg.SetReply(r)
+		msg.Authoritative = true
 
+		domain := msg.Question[0].Name
 		if h.check(domain) {
 			w.WriteMsg(&msg) // just sink right now
 			return
 		}
 	}
 
-	go h.forward(&w, r, &msg, "1.1.1.1")
+	go h.forward(&w, r, "1.1.1.1:53")
 }
